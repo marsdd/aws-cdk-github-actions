4ed46d4bebf7b9ac506e3125fa215040c3edf243
Marlon Murray <43752281+mmurraymarsdd@users.noreply.github.com>
2021-02-10 11:33:51 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,6 @@ function runCdk(){
 	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
 	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1)
 	exitCode=${?}
-	echo exitCode=$exitCode
 	echo ::set-output name=status_code::${exitCode}
 	echo "${output}"
 

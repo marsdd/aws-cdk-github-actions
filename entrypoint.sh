ce347e161747bd2d8c33a2c9ad182e0a4bf77afd
Marlon Murray <43752281+mmurraymarsdd@users.noreply.github.com>
2021-02-10 12:08:08 -0500

    update entrypoint runCdk

--- entrypoint.sh
+++ entrypoint.sh
@@ -71,8 +71,9 @@ function installNodeModules(){
 
 function runCdk(){
 	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
-	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1)
+	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}")
 	exitCode=${?}
+	echo exitCode=$exitCode
 	echo ::set-output name=status_code::${exitCode}
 	echo "${output}"
 
